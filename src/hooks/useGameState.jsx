0a83caf43f13949b5abceea7b63781f8fb109dd1
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-19 14:25:27 +0100

    Fix: subscribing to socket events multiple times whenever a game is joined

--- src/hooks/useGameState.jsx
+++ src/hooks/useGameState.jsx
@@ -3,12 +3,37 @@ import { useState, useEffect } from 'react';
 export default function useGameState(socket) {
   const [gameState, setGameState] = useState(null);
 
-  // Want this to only run if the socket is changed.
+  // Want this to only run if the socket has changed.
   useEffect(() => {
     if (socket) {
-      socket.on('game_state_updated', (updatedGameState) => {
-        setGameState(updatedGameState);
-      });
+      function updateGameState(updates) {
+        setGameState(updates);
+      }
+
+      socket.on('game_started', updateGameState);
+      socket.on('game_round_started', updateGameState);
+      socket.on('game_round_countdown_started', updateGameState);
+      socket.on('game_round_countdown_updated', updateGameState);
+      socket.on('game_state_updated', updateGameState);
+      socket.on('game_round_ended', updateGameState);
+      socket.on('game_round_failed', updateGameState);
+      socket.on('game_over', updateGameState);
+      socket.on('single_player_game_over', updateGameState);
+      socket.on('game_ended', updateGameState);
+
+      // Unsubscribe from game events on unmount, before going back to lobby screen.
+      return () => {
+        socket.off('game_started', updateGameState);
+        socket.off('game_round_started', updateGameState);
+        socket.off('game_round_countdown_started', updateGameState);
+        socket.off('game_round_countdown_updated', updateGameState);
+        socket.off('game_state_updated', updateGameState);
+        socket.off('game_round_ended', updateGameState);
+        socket.off('game_round_failed', updateGameState);
+        socket.off('game_over', updateGameState);
+        socket.off('single_player_game_over', updateGameState);
+        socket.off('game_ended', updateGameState);
+      };
     }
   }, [socket]);
 
