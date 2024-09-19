01278a65675a8740d5a499cc2d0311e486e6712b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-19 14:31:41 +0100

    Fix: use socket for deployment version

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ import LobbyScreen from './components/LobbyScreen';
 import GameScreen from './components/GameScreen';
 
 function App() {
-  const socket = useSocket(import.meta.env.VITE_SERVER_URL);
+  // I'd really like it if we could have this in some kind of environment variable,
+  // but when this client dist version is built, the vite env variable gets baked in.
+
+  // A socket is created here and passed down via context, as we want one socket for the whole application.
+  // If we called useSocket everywhere we wanted to use a socket, we would end up creating a whole bunch of sockets.
+  // const socket = useSocket('http://localhost:25565');
+  const socket = useSocket('https://pac-snake-online.adaptable.app:443');
   const uuid = useUUID(socket);
   const clientMetadata = useClientMetadata(socket, uuid);
 
