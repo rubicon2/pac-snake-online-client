1400afdb384c9aa640b727f99b645d31a0f1236e
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-20 12:52:19 +0100

    Improve look of food gameobjects

--- src/components/Food/index.jsx
+++ src/components/Food/index.jsx
@@ -1,17 +1,47 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { generateRandomisedRGBAStrings } from '../../rgba';
 
 const FoodChunk = styled.div.attrs((props) => ({
   style: {
     left: `${props.$x * props.$cellSize + props.$cellSize * 0.3125}px`,
     top: `${props.$y * props.$cellSize + props.$cellSize * 0.3125}px`,
+    backgroundColor: props.$backgroundColor,
   },
 }))`
   position: absolute;
-  background-color: green;
   width: ${(props) => `${props.$cellSize * 0.375}px`};
   height: ${(props) => `${props.$cellSize * 0.375}px`};
+  animation: spin 2s linear 0ms infinite;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0);
+      scale: 0.8;
+    }
+    50% {
+      scale: 0.9;
+    }
+    to {
+      transform: rotate(180deg);
+      scale: 0.8;
+    }
+  }
 `;
 
 export default function Food({ cellSize, x = 0, y = 0 }) {
-  return <FoodChunk $cellSize={cellSize} $x={x} $y={y} />;
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    setColors(generateRandomisedRGBAStrings({ r: 30, g: 230, b: 30, a: 1 }));
+  }, []);
+
+  return (
+    <FoodChunk
+      $cellSize={cellSize}
+      $x={x}
+      $y={y}
+      $backgroundColor={colors[Math.floor((colors.length - 1) * Math.random())]}
+    />
+  );
 }
