02742342c3cafb8808ef3c63527fe97712bd4dd9
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-20 19:34:16 +0100

    Fix: useMessages only giving the last message received instead of the last 5

--- src/hooks/useMessages.jsx
+++ src/hooks/useMessages.jsx
@@ -1,32 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function useMessages(socket) {
   const [messages, setMessages] = useState([]);
-
-  // Why does this work ok here, but not in a useEffect function?
-  // This adds a ridiculous number of 'message_received' callbacks to the socket, as expected!
-  // But why does it work anyway?
-  // if (socket) {
-  //   socket.on('message_received', (message) => {
-  //     const time = new Date(Date.now()).toLocaleTimeString();
-  //     const newMessage = `${time} - ${message}`;
-  //     setMessages([
-  //       newMessage,
-  //       ...messages.filter((message, index) => index < 5),
-  //     ]);
-  //   });
-  // }
+  const existingMessages = useRef([]);
 
   // Doesn't append to messages state array properly if it is in the useEffect function... why?
+  // It was because the callback passed to socket.on() used the constant value when useMessages is first called - an empty array.
+  // It does not get the updated version when setMessages is called. It would only get a new version if the socket changes and
+  // the useEffect runs again. Have used a ref to get around this, to get the latest value, but setMessages is still required
+  // for the dependent components to rerender with the new message. This is clunky and dodgy and there must be a better way to do it.
   useEffect(() => {
     if (socket) {
       socket.on('message_received', (message) => {
         const time = new Date(Date.now()).toLocaleTimeString();
         const newMessage = `${time} - ${message}`;
-        setMessages([
-          newMessage,
-          ...messages.filter((message, index) => index < 5),
-        ]);
+        existingMessages.current.unshift(newMessage);
+        // Limit to five messages.
+        while (existingMessages.current.length > 5)
+          existingMessages.current.pop();
+        setMessages(existingMessages.current);
       });
     }
   }, [socket]);
