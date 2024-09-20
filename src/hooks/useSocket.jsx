59c9a35c5f9068f1a1074262ffbc0a3c8baa7c7d
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-09-20 16:08:25 +0100

    Reduce lag  when reconnecting to server

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -1,13 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
+
+const HTTP_TIMEOUT_MS = parseInt(import.meta.env.VITE_SERVER_HTTP_TIMEOUT_MS);
 
 export default function useSocket(url) {
   const [socket, setSocket] = useState(null);
+  const oldSocket = useRef(null);
+  const reconnectTimeout = useRef(null);
 
   useEffect(() => {
-    setSocket(io(url));
+    // Platform as a service will have a set http timeout.
+    // The purpose of this is to set up a new socket before the old one times out,
+    // then replace the old socket with the new one, once it is all set up and ready to go.
+    // This should reduce (or, hopefully, eliminate) lag when the http timeout occurs.
+    function connect() {
+      const newSocket = io(url);
+      newSocket.on('connect', () => {
+        if (oldSocket.current) oldSocket.current.close();
+        oldSocket.current = newSocket;
+        // Trigger rerender, so any components that use this hook will have the new socket.
+        setSocket(newSocket);
+        reconnectTimeout.current = setTimeout(connect, HTTP_TIMEOUT_MS);
+      });
+    }
+
+    connect();
+
     return () => {
-      setSocket(null);
+      if (oldSocket.current) oldSocket.current.close();
+      oldSocket.current = null;
+      clearTimeout(reconnectTimeout.current);
+      reconnectTimeout.current = null;
     };
   }, [url]);
 
